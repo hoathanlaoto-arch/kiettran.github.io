c6d7dbe003d215baafde64a43d2f854be9f4213f
hoathanlaoto-arch <hoathanlaoto@gmail.com>
2025-09-25 22:56:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 function chao() {
-  alert("Tôi là Trần Gia Kiệt vua dev 9N1 🚀");
+  alert("Tôi là Trần Gia Kiệt - Vua DEV 9N1 😎🚀");
 }
+
+// Animation khi scroll vào phần tử
+const cards = document.querySelectorAll(".card");
+
+function checkScroll() {
+  const triggerBottom = window.innerHeight * 0.8;
+  cards.forEach(card => {
+    const cardTop = card.getBoundingClientRect().top;
+    if (cardTop < triggerBottom) {
+      card.classList.add("show");
+    }
+  });
+}
+
+window.addEventListener("scroll", checkScroll);
+window.addEventListener("load", checkScroll);
